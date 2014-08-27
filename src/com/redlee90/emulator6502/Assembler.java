3e207113d9220a8d4f6559c9bf4594b7399a60ce
Rui Li <redlee90@gmail.com>
2014-08-27 17:58:59 -0500

    Add part of VM implementation

--- src/com/redlee90/emulator6502/Assembler.java
+++ src/com/redlee90/emulator6502/Assembler.java
@@ -12,10 +12,23 @@ public class Assembler {
 	private Memory memory;
 
 	private HashMap<String, String> LDA;
+	private HashMap<String, String> LDX;
+	private HashMap<String, String> LDY;
+	private HashMap<String, String> INC;
 	private HashMap<String, String> STA;
 
 	public Assembler(Memory memory) {
 		this.memory = memory;
+
+		INC = new HashMap<String, String>() {
+			{
+				put("ZP", "e6");
+				put("ZPX", "f6");
+				put("ABS", "ee");
+				put("ABSX", "fe");
+			}
+		};
+
 		LDA = new HashMap<String, String>() {
 			{
 				put("Imm", "a9");
@@ -27,6 +40,27 @@ public class Assembler {
 				put("INDX", "a1");
 				put("INDY", "b1");
 			}
+		};
+
+		LDX = new HashMap<String, String>() {
+			{
+				put("Imm", "a2");
+				put("ZP", "a6");
+				put("ZPY", "b6");
+				put("ABS", "ae");
+				put("ABSY", "be");
+			}
+		};
+
+		LDY = new HashMap<String, String>() {
+			{
+				put("Imm", "a0");
+				put("ZP", "a4");
+				put("ZPX", "b4");
+				put("ABS", "ac");
+				put("ABSX", "bc");
+			}
+
 		};
 
 		STA = new HashMap<String, String>() {
@@ -41,6 +75,7 @@ public class Assembler {
 
 			}
 		};
+
 	}
 
 	private void restore() {
@@ -61,7 +96,7 @@ public class Assembler {
 	}
 
 	private void assembleLine(String line) {
-		String patternImm = "(\\s*)(\\w{3})(\\s*)(#\\$)([a-f,0-9]{2})(\\s*;?.*)";	
+		String patternImm = "(\\s*)(\\w{3})(\\s*)(#\\$)([a-f,0-9]{2})(\\s*;?.*)";
 		String patternZP = "(\\s*)(\\w{3})(\\s*)(\\$)([a-f,0-9]{2})([$,\\s*;.*])";
 		String patternZPX = "(\\s*)(\\w{3})(\\s*)(\\$)([a-f,0-9]{2})(\\s*,\\s*)([X,x])(\\s*;?.*)";
 		String patternZPY = "(\\s*)(\\w{3})(\\s*)(\\$)([a-f,0-9]{2})(\\s*,\\s*)([Y,y])(\\s*;?.*)";
@@ -74,7 +109,7 @@ public class Assembler {
 
 		// Imm
 		if (line.matches(patternImm)) {
-			System.out.println(line+" matches Imm");
+			System.out.println(line + " matches Imm");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternImm, "$2 $5"));
 			String command = st.nextToken();
@@ -86,8 +121,18 @@ public class Assembler {
 				this.memory.cells[defaultPC++] = ImmNum;
 				size += 2;
 				break;
+			case "LDX":
+				this.memory.cells[defaultPC++] = LDX.get("Imm");
+				this.memory.cells[defaultPC++] = ImmNum;
+				size += 2;
+				break;
+			case "LDY":
+				this.memory.cells[defaultPC++] = LDY.get("Imm");
+				this.memory.cells[defaultPC++] = ImmNum;
+				size += 2;
+				break;
 			default:
-				this.assembleOK=false;
+				this.assembleOK = false;
 				break;
 			}
 
@@ -95,14 +140,29 @@ public class Assembler {
 
 		// ZP
 		else if (line.matches(patternZP)) {
-			System.out.println(line+" matches ZP");
+			System.out.println(line + " matches ZP");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(patternZP,
 					"$2 $5"));
 			String command = st.nextToken();
 			String ZPAddr = st.nextToken();
 			switch (command) {
+			case "INC":
+				this.memory.cells[defaultPC++] = INC.get("ZP");
+				this.memory.cells[defaultPC++] = ZPAddr;
+				size += 2;
+				break;
 			case "LDA":
 				this.memory.cells[defaultPC++] = LDA.get("ZP");
+				this.memory.cells[defaultPC++] = ZPAddr;
+				size += 2;
+				break;
+			case "LDX":
+				this.memory.cells[defaultPC++] = LDX.get("ZP");
+				this.memory.cells[defaultPC++] = ZPAddr;
+				size += 2;
+				break;
+			case "LDY":
+				this.memory.cells[defaultPC++] = LDY.get("ZP");
 				this.memory.cells[defaultPC++] = ZPAddr;
 				size += 2;
 				break;
@@ -119,14 +179,24 @@ public class Assembler {
 
 		// ZPX
 		else if (line.matches(patternZPX)) {
-			System.out.println(line+" matches ZPX");
+			System.out.println(line + " matches ZPX");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternZPX, "$2 $5"));
 			String command = st.nextToken();
 			String ZPXAddr = st.nextToken();
 			switch (command) {
+			case "INC":
+				this.memory.cells[defaultPC++] = INC.get("ZPX");
+				this.memory.cells[defaultPC++] = ZPXAddr;
+				size += 2;
+				break;
 			case "LDA":
 				this.memory.cells[defaultPC++] = LDA.get("ZPX");
+				this.memory.cells[defaultPC++] = ZPXAddr;
+				size += 2;
+				break;
+			case "LDY":
+				this.memory.cells[defaultPC++] = LDY.get("ZPX");
 				this.memory.cells[defaultPC++] = ZPXAddr;
 				size += 2;
 				break;
@@ -144,14 +214,14 @@ public class Assembler {
 
 		// ZPY
 		else if (line.matches(patternZPY)) {
-			System.out.println(line+" matches ZPY");
+			System.out.println(line + " matches ZPY");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternZPY, "$2 $5"));
 			String command = st.nextToken();
 			String ZPYAddr = st.nextToken();
 			switch (command) {
-			case "LDA":
-				this.memory.cells[defaultPC++] = LDA.get("ZPY");
+			case "LDX":
+				this.memory.cells[defaultPC++] = LDX.get("ZPY");
 				this.memory.cells[defaultPC++] = ZPYAddr;
 				size += 2;
 				break;
@@ -164,14 +234,32 @@ public class Assembler {
 
 		// ABS
 		else if (line.matches(patternABS)) {
-			System.out.println(line+" matches ABS");
+			System.out.println(line + " matches ABS");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternABS, "$2 $5"));
 			String command = st.nextToken();
 			String ABSAddr = st.nextToken();
 			switch (command) {
+			case "INC":
+				this.memory.cells[defaultPC++] = INC.get("ABS");
+				this.memory.cells[defaultPC++] = ABSAddr.substring(2);
+				this.memory.cells[defaultPC++] = ABSAddr.substring(0, 2);
+				this.size += 3;
+				break;
 			case "LDA":
 				this.memory.cells[defaultPC++] = LDA.get("ABS");
+				this.memory.cells[defaultPC++] = ABSAddr.substring(2);
+				this.memory.cells[defaultPC++] = ABSAddr.substring(0, 2);
+				this.size += 3;
+				break;
+			case "LDX":
+				this.memory.cells[defaultPC++] = LDX.get("ABS");
+				this.memory.cells[defaultPC++] = ABSAddr.substring(2);
+				this.memory.cells[defaultPC++] = ABSAddr.substring(0, 2);
+				this.size += 3;
+				break;
+			case "LDY":
+				this.memory.cells[defaultPC++] = LDY.get("ABS");
 				this.memory.cells[defaultPC++] = ABSAddr.substring(2);
 				this.memory.cells[defaultPC++] = ABSAddr.substring(0, 2);
 				this.size += 3;
@@ -190,14 +278,26 @@ public class Assembler {
 
 		// ABSX
 		else if (line.matches(patternABSX)) {
-			System.out.println(line+" matches ABSX");
+			System.out.println(line + " matches ABSX");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternABSX, "$2 $5"));
 			String command = st.nextToken();
 			String ABSXAddr = st.nextToken();
 			switch (command) {
+			case "INC":
+				this.memory.cells[defaultPC++] = INC.get("ABSX");
+				this.memory.cells[defaultPC++] = ABSXAddr.substring(2);
+				this.memory.cells[defaultPC++] = ABSXAddr.substring(0, 2);
+				this.size += 3;
+				break;
 			case "LDA":
 				this.memory.cells[defaultPC++] = LDA.get("ABSX");
+				this.memory.cells[defaultPC++] = ABSXAddr.substring(2);
+				this.memory.cells[defaultPC++] = ABSXAddr.substring(0, 2);
+				this.size += 3;
+				break;
+			case "LDY":
+				this.memory.cells[defaultPC++] = LDY.get("ABSX");
 				this.memory.cells[defaultPC++] = ABSXAddr.substring(2);
 				this.memory.cells[defaultPC++] = ABSXAddr.substring(0, 2);
 				this.size += 3;
@@ -217,7 +317,7 @@ public class Assembler {
 
 		// ABSY
 		else if (line.matches(patternABSY)) {
-			System.out.println(line+" matches ABSY");
+			System.out.println(line + " matches ABSY");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternABSY, "$2 $5"));
 			String command = st.nextToken();
@@ -225,6 +325,12 @@ public class Assembler {
 			switch (command) {
 			case "LDA":
 				this.memory.cells[defaultPC++] = LDA.get("ABSY");
+				this.memory.cells[defaultPC++] = ABSYAddr.substring(2);
+				this.memory.cells[defaultPC++] = ABSYAddr.substring(0, 2);
+				this.size += 3;
+				break;
+			case "LDX":
+				this.memory.cells[defaultPC++] = LDX.get("ABSY");
 				this.memory.cells[defaultPC++] = ABSYAddr.substring(2);
 				this.memory.cells[defaultPC++] = ABSYAddr.substring(0, 2);
 				this.size += 3;
@@ -243,7 +349,7 @@ public class Assembler {
 		}
 		// IND
 		else if (line.matches(patternIND)) {
-			System.out.println(line+" matches IND");
+			System.out.println(line + " matches IND");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternIND, "$2 $6"));
 			String command = st.nextToken();
@@ -253,7 +359,7 @@ public class Assembler {
 
 		// INDX
 		else if (line.matches(patternINDX)) {
-			System.out.println(line+" matches INDX");
+			System.out.println(line + " matches INDX");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternINDX, "$2 $6"));
 			String command = st.nextToken();
@@ -277,7 +383,7 @@ public class Assembler {
 
 		// INDY
 		else if (line.matches(patternINDY)) {
-			System.out.println(line+" matches INDY");
+			System.out.println(line + " matches INDY");
 			StringTokenizer st = new StringTokenizer(line.replaceAll(
 					patternINDY, "$2 $6"));
 			String command = st.nextToken();
--- src/com/redlee90/emulator6502/VM.java
+++ src/com/redlee90/emulator6502/VM.java
@@ -1,11 +1,42 @@
 package com.redlee90.emulator6502;
 
 public class VM {
-	private int regA;
-	private int regX;
-	private int regY;
-	private int regP;
+	private String regA;
+	private String regX;
+	private String regY;
+	private String regP;
 	private int regPC = 0x600;
 	private int regSP = 0xff;
+	private Memory memory;
+	
+	public VM (Memory memory) {
+		this.memory = memory;
+	}
+	
+	public void run() {
+		String command = this.memory.cells[regPC++];
+		switch (command) {
+		case "85":
+			this.memory.cells[Integer.parseInt(this.memory.cells[regPC++],16)] = this.regA;
+			break;
+		case "a0":
+			this.regY = this.memory.cells[regPC++];
+			break;
+		case "a2":
+			this.regX = this.memory.cells[regPC++];
+			break;	
+		case "a5":
+			this.regA = this.memory.cells[Integer.parseInt(this.memory.cells[regPC++],16)];
+		case "a9":
+			this.regA = this.memory.cells[regPC++];
+			break;
+		
+		default:
+			break;
+		
+		}
+				
+	}
+	
 
 }
